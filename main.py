186dbeec3b8e2cdcbe13b1b6902f1efd97f073dc
Tanuutka <Tanuutka@yandex.ru>
2023-01-24 17:54:31 +0300

    создан автопарк

---
+++ main.py
@@ -0,0 +1,15 @@
+import function as fn
+from menu import Menu
+if __name__ == "__main__":
+#     основной блок
+    menuitems = [
+        ("1", "Вывод автобусов", fn.print_bus),
+        ("2", "Добавление автобуса", fn.add_bus),
+        ("3", "Вывод водителей", fn.print_driver),
+        ("4", "Добавление водителей", fn.add_driver),
+        ("5", "Вывод маршрута", fn.print_route),
+        ("6", "Добавление маршрута", fn.add_route),
+        ("7", "Выход", lambda: exit())]
+
+    menu = Menu(menuitems)
+    menu.run('>:')  
